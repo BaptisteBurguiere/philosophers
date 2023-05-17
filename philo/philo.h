9f820b4e6b5cb8fa0d314f420fa9f93e9065e932
Baptiste Burguiere <bburguie@c1r3p7.42lehavre.fr>
2023-05-17 17:20:28 +0200

    pas mal

--- philo/philo.h
+++ philo/philo.h
@@ -21,20 +21,6 @@
 
 # define MAX "4294967295"
 
-typedef struct s_philo
-{
-	unsigned int	total;
-	unsigned int	ttd;
-	unsigned int	tte;
-	unsigned int	tts;
-	unsigned int	me;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	*term;
-	mutex_t			*is_dead;
-	mutex_t			*has_all_eaten;
-	mutex_t			ttd_mutex;
-}	t_philo;
-
 // typedef struct s_arg
 // {
 // 	unsigned int	n;
@@ -60,22 +46,39 @@ typedef struct s_mutex
 	pthread_mutex_t	mutex;
 }	t_mutex;
 
+typedef struct s_philo
+{
+	unsigned int	total;
+	unsigned int	ttd;
+	unsigned int	tte;
+	unsigned int	tts;
+	unsigned int	me;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*term;
+	t_mutex			*is_dead;
+	t_mutex			*has_all_eaten;
+}	t_philo;
+
 typedef struct s_arg
 {
 	unsigned int	n;
-	mutex_t			has_eaten;
-	t_philo			vars;
+	unsigned int	has_eaten;
+	t_mutex			ttd_mutex;
+	t_philo			*vars;
 }	t_arg;
-
-typedef struct s_
 
 int				ft_strcmp(const char *s1, const char *s2);
 unsigned int	ft_atoi(const char *nptr);
 long long		get_time(void);
-void			ft_print(t_arg, *arg, const char *mess);
+void			ft_print(t_arg *arg, const char *mess, long long time);
 
 int				is_args(int argc, char const *argv[], t_philo *vars);
 
 void			*philo_routine(void *data);
+int				is_over(t_arg *arg);
+void			*philo_ttd(void *data);
+int				split_1(t_arg *arg);
+int				split_2(t_arg *arg);
+int				split_3(t_arg *arg);
 
 #endif
--- philo/utils.c
+++ philo/utils.c
@@ -73,9 +73,7 @@ long long	get_time(void)
 	return (time_ms - base_time);
 }
 
-void	ft_print(t_arg, *arg, const char *mess)
+void	ft_print(t_arg *arg, const char *mess, long long time)
 {
-	pthread_mutex_lock(arg->vars->term);
-	printf("%lld %u %s\n", get_time(), arg->n + 1, mess);
-	pthread_mutex_unlock(arg->vars->term);
+	printf("%lld %u %s\n", time, arg->n + 1, mess);
 }
